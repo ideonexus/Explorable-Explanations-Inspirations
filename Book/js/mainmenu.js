888659a9880ee9213e665544074f7208955f07ac
Ryan Somma <ideonexus@gmail.com>
2017-02-08 15:34:27 -0500

    Initial Checkin.

---
+++ Book/js/mainmenu.js
@@ -0,0 +1,17 @@
+
+var headerHtml = '<div id="headLinks"><a href="index.html">Home</a>'
+ + ' | <a href="00Introduction.html">00</a> '
+ + ' | <a href="01BenefitsOfCoding.html">01</a>'
+ + ' | <a href="02ExplorableExplanations.html">02</a>'
+ + ' | <a href="03CodeAppreciation_3000YearHistory.html">03</a>'
+ + ' | <a href="04CodeAppreciation_CodingConcepts.html">04</a>'
+ + ' | <a href="05HackerVirtues.html">05</a>'
+ + ' | <a href="06OurEnvironment.html">06</a>'
+ + ' | <a href="07CodingOnTheStackOfGiants.html">07</a>'
+ + ' | <a href="08LetYourStudentsLead.html">08</a>'
+ + ' | <a href="09IsCodingTheNewLiteracy.html">09</a>'
+ + ' | <a href="10Attributions.html">10</a>'
+ + '</div>';
+$( document ).ready(function() {
+    $("#headLinks").html(headerHtml);
+});
